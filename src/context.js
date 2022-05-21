0ba886e76ae6d4eed04f13f8c0ca8a58ff90657f
ReyHaan <hodekarehan@gmail.com>
2022-05-21 13:53:40 +0530

    bug fix 2

--- src/context.js
+++ src/context.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
-// console.log(API_ENDPOINT);
 
 const AppContext = React.createContext();
 
